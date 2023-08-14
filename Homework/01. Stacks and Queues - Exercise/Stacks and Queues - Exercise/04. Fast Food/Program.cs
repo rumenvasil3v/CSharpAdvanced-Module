e8cde46aeb0417942cc902c9b7433d8bea2dd90c
Rumen <rumenstvass@gmail.com>
2023-08-14 01:30:26 +0100

    Solving Fast Food

---
+++ Homework/01. Stacks and Queues - Exercise/Stacks and Queues - Exercise/04. Fast Food/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _04._Fast_Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // INPUT /Quantity of foof/
+            int quantityOfFood = int.Parse(Console.ReadLine());
+
+            // INPUT /array of integers/
+            int[] arrayOfIntegers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> queue = new Queue<int>(arrayOfIntegers);
+
+            // CODE LOGIC
+            Console.WriteLine(queue.Max());
+
+            DequeueOrders(queue, quantityOfFood, arrayOfIntegers); // dequeue orders from the queue
+            PrintInformationAboutOrders(queue); // print if it has orders or if it does not have
+        }
+
+        static void DequeueOrders(Queue<int> queue, int quantityOfFood, int[] arrayOfIntegers)
+        {
+            for (int n = 0; n < arrayOfIntegers.Length; n++)
+            {
+                if (quantityOfFood - arrayOfIntegers[n] >= 0) // check if we have enough food to execute the order
+                {
+                    quantityOfFood -= arrayOfIntegers[n];
+                    queue.Dequeue();
+                }
+                else
+                {
+                    break; // if we don't have food, break and print remaining orders
+                }
+            }
+        }
+
+        static void PrintInformationAboutOrders(Queue<int> queue)
+        {
+            // OUTPUT
+
+            if (queue.Count == 0) // check if queue count equals zero
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", queue)}"); // if count is > 0, print remaining orders
+            }
+        }
+    }
+}
---
+++ Homework/01. Stacks and Queues - Exercise/Stacks and Queues - Exercise/05. Fashion Boutique/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _05._Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
