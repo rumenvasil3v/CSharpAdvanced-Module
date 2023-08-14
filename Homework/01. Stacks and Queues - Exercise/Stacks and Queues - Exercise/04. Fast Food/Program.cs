a7dcd9f07bbca9a77ea92ce1919c55993f5440bf
Rumen <rumenstvass@gmail.com>
2023-08-14 01:30:26 +0100

    Solving Fast Food

---
+++ Homework/01. Stacks and Queues - Exercise/Stacks and Queues - Exercise/04. Fast Food/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _04._Fast_Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityOfFood = int.Parse(Console.ReadLine());
+
+            int[] arrayOfIntegers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> queue = new Queue<int>(arrayOfIntegers);
+
+            Console.WriteLine(queue.Max());
+
+            DequeueOrders(queue, quantityOfFood, arrayOfIntegers);
+            PrintInformationAboutOrders(queue);
+        }
+
+        static void DequeueOrders(Queue<int> queue, int quantityOfFood, int[] arrayOfIntegers)
+        {
+            for (int n = 0; n < arrayOfIntegers.Length; n++)
+            {
+                if (quantityOfFood - arrayOfIntegers[n] >= 0)
+                {
+                    quantityOfFood -= arrayOfIntegers[n];
+                    queue.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        static void PrintInformationAboutOrders(Queue<int> queue)
+        {
+            if (queue.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", queue)}");
+            }
+        }
+    }
+}
