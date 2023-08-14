7937a932fd3518067f8b5c814c85b51637ee2613
Rumen <rumenstvass@gmail.com>
2023-08-14 14:21:23 +0100

    Modifying Exercise

--- Homework/01. Stacks and Queues - Exercise/Stacks and Queues - Exercise/07. Truck Tour/Program.cs
+++ Homework/01. Stacks and Queues - Exercise/Stacks and Queues - Exercise/07. Truck Tour/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int numberOfPetrolPumps = int.Parse(Console.ReadLine());
+
+            PetrolPumps(numberOfPetrolPumps);
+        }
+
+        static void PetrolPumps(int numberOfPetrolPumps)
+        {
+            int smallest = int.MaxValue;
+            Stack<int> stack = new Stack<int>();
+
+            for (int n = 0; n < numberOfPetrolPumps; n++)
+            {
+                int[] arrayOfIntegers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                int amountOfPetrol = arrayOfIntegers[0];
+                int distanceInKiolometers = arrayOfIntegers[1];
+                stack.Push(n);
+
+                for (int d = 0; d < distanceInKiolometers; d++)
+                {
+                    amountOfPetrol--;
+                }
+
+                if (amountOfPetrol > 0)
+                {
+                    if (smallest > n)
+                    {
+                        smallest = n;
+                    }
+                }
+            }
+
+            Console.WriteLine(smallest);
         }
     }
 }
