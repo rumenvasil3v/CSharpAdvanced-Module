6712cb507d1df20ddb56d672daab349e424213b2
Rumen <rumenstvass@gmail.com>
2023-08-15 01:32:26 +0100

    Solving Balanced Parentheses

---
+++ Homework/01. Stacks and Queues - Exercise/Stacks and Queues - Exercise/07. Truck Tour - Exercise/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace _07_Truck_Tour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var queueTruck = new Queue<int>();
+
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                var information = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+                int amountOfPetrol = information[0];
+                int distance = information[1];
+
+                queueTruck.Enqueue(amountOfPetrol - distance);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (queueTruck.Peek() >= 0)
+                {
+                    foreach (var elm in queueTruck)
+                    {
+                        sum += elm;
+                        if (sum < 0)
+                        {
+                            break;
+                        }
+                    }
+
+                    if (sum >= 0)
+                    {
+                        Console.WriteLine(i);
+                        return;
+                    }
+                    else
+                    {
+                        queueTruck.Enqueue(queueTruck.Peek());
+                        queueTruck.Dequeue();
+                        sum = 0;
+                    }
+                }
+                else
+                {
+                    queueTruck.Enqueue(queueTruck.Peek());
+                    queueTruck.Dequeue();
+                }
+            }
+        }
+    }
+}
---
+++ Homework/01. Stacks and Queues - Exercise/Stacks and Queues - Exercise/08. Balanced Parenthesis/Program.cs
@@ -0,0 +1,99 @@
+﻿namespace _08._Balanced_Parenthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sequenceOfParenthesis = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+
+            IterateThroughParenthesis(stack, sequenceOfParenthesis);
+        }
+
+        static void IterateThroughParenthesis(Stack<char> stack, string sequenceOfParenthesis)
+        {
+            char closingBracket = '\0';
+
+            bool isFalse = false;
+            for (int n = 0; n < sequenceOfParenthesis.Length; n++)
+            {
+                char currentChar = sequenceOfParenthesis[n];
+
+                switch (currentChar)
+                {
+                    case '(':
+                        stack.Push(sequenceOfParenthesis[n]);
+                        break;
+                    case '{':
+                        stack.Push(sequenceOfParenthesis[n]);
+                        break;
+                    case '[':
+                        stack.Push(sequenceOfParenthesis[n]);
+                        break;
+                    case ']':
+                        if (stack.Count == 0)
+                        {
+                            isFalse = true;
+                            break;
+                        }
+                        
+                        if (stack.Pop() == '[')
+                        {
+                            continue;
+                        }
+
+                        isFalse = true;
+                        break;
+                    case '}':
+                        if (stack.Count == 0)
+                        {
+                            isFalse = true;
+                            break;
+                        }
+                        
+                        if (stack.Pop() == '{')
+                        {
+                            continue;
+                        }
+
+                        isFalse = true;
+                        break;
+                    case ')':
+                        if (stack.Count == 0)
+                        {
+                            isFalse = true;
+                            break;
+                        }
+                        
+                        if (stack.Pop() == '(')
+                        {
+                            continue;
+                        }
+
+                        isFalse = true;
+                        break;
+                }
+
+                if (isFalse)
+                {
+                    break;
+                }
+            }
+
+            CheckIfParenthesisAreBalanced(stack, isFalse);
+        }
+
+        static void CheckIfParenthesisAreBalanced(Stack<char> stack, bool isFalse)
+        {
+            if (!isFalse && stack.Count == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
