f17771cadd5e029033d0821cae42123a665004b6
Rumen <rumenstvass@gmail.com>
2023-08-15 17:57:53 +0100

    Solving Crossroads Exercise

---
+++ Homework/01. Stacks and Queues - Exercise/Stacks and Queues - Exercise/10. Crossroads - Exercise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _10._Crossroads___Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- Homework/01. Stacks and Queues - Exercise/Stacks and Queues - Exercise/10. Crossroads/Program.cs
+++ Homework/01. Stacks and Queues - Exercise/Stacks and Queues - Exercise/10. Crossroads/Program.cs
@@ -1,10 +1,120 @@
-﻿namespace _10._Crossroads
+﻿using System.Net;
+
+namespace _10._Crossroads
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            int durationOfGreenLight = int.Parse(Console.ReadLine());
+            int freeWindow = int.Parse(Console.ReadLine());
+
+            Queue<string> queue = new Queue<string>();
+            Stack<string> stack = new Stack<string>();
+            ManipulateTraffic(stack, queue, durationOfGreenLight, freeWindow);
+        }
+
+        static void ManipulateTraffic(Stack<string> stack, Queue<string> queue, int durationOfGreenLight, int freeWindow)
+        {
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                switch (command)
+                {
+                    case "green":
+                        bool isThereAccident = CarPassingThroughCrossroad(stack, queue, durationOfGreenLight, freeWindow);
+                       
+                        if (!isThereAccident)
+                        {
+                            return;
+                        }
+
+                        break;
+                    default:
+                        string car = command;
+
+                        EnqueueCarToTraffic(queue, car);
+                        break;
+                }
+            }
+
+            Console.WriteLine("Everyone is safe.");
+            Console.WriteLine($"{stack.Count} total cars passed the crossroads.");
+
+        }
+
+        static void EnqueueCarToTraffic(Queue<string> queue, string car)
+        {
+            queue.Enqueue(car);
+        }
+
+        static bool CarPassingThroughCrossroad(Stack<string> stack, Queue<string> queue, int durationOfGreenLight, int freeWindow)
+        {
+            bool noAccident = false;
+
+            int secondsGreenLight = durationOfGreenLight;
+            int secondsFreeWindow = freeWindow;
+            int count = queue.Count;
+
+            for (int n = 0; n < count; n++)
+            {
+
+                if (secondsGreenLight == 0)
+                {
+                    break;
+                }
+
+                bool carPassedSuccessfully = false;
+                noAccident = false;
+
+                string currentCar = queue.Dequeue();
+                stack.Push(currentCar);
+
+                Queue<char> carQueue = new Queue<char>(currentCar);
+
+                while (secondsGreenLight > 0)
+                {
+                    secondsGreenLight--;
+                    carQueue.Dequeue();
+                    if (carQueue.Count == 0)
+                    {
+                        carPassedSuccessfully = true;
+                        break;
+                    }
+                }
+
+                if (carPassedSuccessfully)
+                {
+                    noAccident = true;
+                    continue;
+                }
+                else
+                {
+                    while (secondsFreeWindow > 0)
+                    {
+                        carQueue.Dequeue();
+                        secondsFreeWindow--;
+                        if (carQueue.Count == 0)
+                        {
+                            carPassedSuccessfully = true;
+                            break;
+                        }
+                    }
+
+                    if (carPassedSuccessfully)
+                    {
+                        noAccident = true;
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("A crash happened!");
+                        Console.WriteLine($"{currentCar} was hit at {carQueue.Dequeue()}.");
+                    }
+                }
+            }
+
+            return noAccident;
         }
     }
 }
