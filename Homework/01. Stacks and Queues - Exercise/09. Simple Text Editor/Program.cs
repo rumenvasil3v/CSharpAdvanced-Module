f5933230b1103c5a6484256ecb18db861bf8e924
Rumen <rumenstvass@gmail.com>
2023-08-16 15:39:01 +0100

    Moving exercise path

--- Homework/01. Stacks and Queues - Exercise/09. Simple Text Editor/Program.cs
+++ Homework/01. Stacks and Queues - Exercise/09. Simple Text Editor/Program.cs
@@ -1,19 +1,34 @@
-﻿namespace _09._Simple_Text_Editor
+﻿/*
+9
+1 HelloThere
+3 7
+2 2
+3 5
+4
+3 7
+4
+1 TestPassed
+3 5
+ */
+
+namespace _09._Simple_Text_Editor
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            // INPUT
             int numberOfOperations = int.Parse(Console.ReadLine());
 
-            Stack<string> firstStack = new Stack<string>();
-            Stack<string> secondStack = new Stack<string>();
+            Stack<string> stack = new Stack<string>();
 
-            ManipulateString(firstStack, secondStack, numberOfOperations);
+            // CODE LOGIC
+            ManipulateString(stack, numberOfOperations);
         }
 
-        static void ManipulateString(Stack<string> firstStack, Stack<string> secondStack, int numberOfOperations)
+        static void ManipulateString(Stack<string> stack, int numberOfOperations)
         {
+            // CODE LOGIC
             for (int n = 0; n < numberOfOperations; n++)
             {
                 string[] command = Console.ReadLine().Split();
@@ -23,30 +38,61 @@
                     case "1":
                         string text = command[1];
 
-                        secondStack.Push(string.Empty);
-
-                        PushingCharactersIntoStack(text, firstStack);
+                        PushingCharactersIntoStack(text, stack);
                         break;
                     case "2":
                         int countCharactersToRemove = int.Parse(command[1]);
 
-                        string result = string.Empty;
+                        string modifiedString = string.Empty;
 
-                        
+                        if (stack.Count == 0)
+                        {
+                            break;
+                        }
 
-                        secondStack.Push(result);
+                        string currentText = stack.Peek();
+
+                        for (int t = 0; t < currentText.Length - countCharactersToRemove; t++)
+                        {
+                            modifiedString += currentText[t];
+                        }
+
+                        stack.Push(modifiedString);
                         break;
                     case "3":
+                        int indexToPrint = int.Parse(command[1]);
+
+                        if (stack.Count == 0)
+                        {
+                            break;
+                        }
+
+                        // OUTPUT
+                        Console.WriteLine(stack.Peek()[indexToPrint - 1]);
                         break;
                     case "4":
+                        if (stack.Count == 0)
+                        {
+                            break;
+                        }
+                        stack.Pop();
+
                         break;
                 }
             }
         }
 
-        static void PushingCharactersIntoStack(string text, Stack<string> firstStack)
+        static void PushingCharactersIntoStack(string text, Stack<string> stack)
         {
-            firstStack.Push(text);
+            if (stack.Count > 0)
+            {
+                string currentText = stack.Pop();
+                stack.Push(currentText);
+                currentText += text;
+                stack.Push(currentText);
+                return;
+            }
+            stack.Push(text);
         }
     }
 }
