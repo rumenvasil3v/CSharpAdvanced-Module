bb6bae1787c3a864b46baae837e0a36e70e60003
Rumen <rumenstvass@gmail.com>
2023-08-16 03:03:46 +0100

    Adding Methods to Cups and Bottles Exercise

--- Homework/01. Stacks and Queues - Exercise/Stacks and Queues - Exercise/12. Cups and Bottles/Program.cs
+++ Homework/01. Stacks and Queues - Exercise/Stacks and Queues - Exercise/12. Cups and Bottles/Program.cs
@@ -4,21 +4,27 @@
     {
         static void Main(string[] args)
         {
+            // INPUT
             int[] cupsCapacity = Console.ReadLine().Split().Select(int.Parse).ToArray();
             int[] bottlesWithWater = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
-            Queue<int> queue = new Queue<int>(cupsCapacity);
             Stack<int> stack = new Stack<int>(bottlesWithWater);
+            Queue<int> queue = new Queue<int>(cupsCapacity);
 
+            // CODE LOGIC
+            PouringWaterInCups(stack, queue);
+        }
+
+        static void PouringWaterInCups(Stack<int> stack, Queue<int> queue)
+        {
             int wastedWater = 0;
-            int leftWater = 0;
 
             while (queue.Count > 0)
             {
 
-                if (stack.Count == 0)
+                // POTENTIONAL OUTPUT IF THERE ARE NO MORE BOTTLES PRINT HOW MANY CUPS AREN'T FILLED
+                if (CheckIfThereAreNoMoreBottles(stack, queue))
                 {
-                    Console.WriteLine($"Cups: {string.Join(' ', queue)}");
                     break;
                 }
 
@@ -27,47 +33,78 @@
 
                 if (currentCup > currentBottle)
                 {
-                    while (currentCup > 0)
-                    {
-                        currentCup--;
-                        currentBottle--;
-                        if (currentBottle == 0 && currentCup > 0)
-                        {
-                            if (stack.Count == 0)
-                            {
-                                break;
-                            }
-                            currentBottle = stack.Pop();
-                        }
-                    }
-
-                    wastedWater += currentBottle;
-
-                    if (currentCup <= 0)
-                    {
-                        leftWater = currentBottle;
-                        queue.Dequeue();
-                    }
+                   wastedWater = MoreThanOneBottleToFillTheCup(queue, stack, currentCup, wastedWater, currentBottle);
                 }
                 else if (currentBottle >= currentCup)
                 {
-                    while (currentCup > 0)
-                    {
-                        currentCup--;
-                        currentBottle--;
-                    }
-
-                    wastedWater += currentBottle;
-                    queue.Dequeue();
+                    wastedWater = OneBottleToFillTheCup(queue, currentCup, currentBottle, wastedWater);
                 }
             }
 
+            //OUTPUT
+            PrintOutput(queue, stack);
+
+            Console.WriteLine($"Wasted litters of water: {wastedWater}");
+        }
+
+        static bool CheckIfThereAreNoMoreBottles(Stack<int> stack, Queue<int> queue)
+        {
+            // POTENTIONAL OUTPUT IF THERE ARE NO MORE BOTTLES PRINT HOW MANY CUPS AREN'T FILLED
+            if (stack.Count == 0)
+            {
+                Console.WriteLine($"Cups: {string.Join(' ', queue)}");
+                return true;
+            }
+
+            return false;
+        }
+
+        static int MoreThanOneBottleToFillTheCup(Queue<int> queue, Stack<int> stack, int currentCup, int wastedWater, int currentBottle)
+        {
+            while (currentCup > 0)
+            {
+                currentCup--;
+                currentBottle--;
+                if (currentBottle == 0 && currentCup > 0)
+                {
+                    if (stack.Count == 0)
+                    {
+                        break;
+                    }
+                    currentBottle = stack.Pop();
+                }
+            }
+
+            wastedWater += currentBottle;
+
+            if (currentCup <= 0)
+            {
+                queue.Dequeue();
+            }
+
+            return wastedWater;
+        }
+
+        static int OneBottleToFillTheCup(Queue<int> queue, int currentCup, int currentBottle, int wastedWater)
+        {
+            while (currentCup > 0)
+            {
+                currentCup--;
+                currentBottle--;
+            }
+
+            wastedWater += currentBottle;
+            queue.Dequeue();
+
+            return wastedWater;
+        }
+
+        static void PrintOutput(Queue<int> queue, Stack<int> stack)
+        {
             if (queue.Count == 0)
             {
                 Console.WriteLine($"Bottles: {string.Join(' ', stack)}");
             }
-
-            Console.WriteLine($"Wasted litters of water: {wastedWater}");
         }
     }
 }
