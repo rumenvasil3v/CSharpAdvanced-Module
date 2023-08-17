78fb8f565d608b754874364a7f0ccc1c21d0d1ae
Rumen <rumenstvass@gmail.com>
2023-08-17 23:30:03 +0100

    Debugging Exercises

--- Homework/02. Multidimensional Arrays - Lab/Multidimensional Arrays - Lab/4. Symbol in Matrix/Program.cs
+++ Homework/02. Multidimensional Arrays - Lab/Multidimensional Arrays - Lab/4. Symbol in Matrix/Program.cs
@@ -10,8 +10,27 @@
             int matrixColumns = squareMatrixSize;
 
             char[,] squareMatrix = new char[matrixRows, matrixColumns];
+
+            AddingElementsToMatrix(squareMatrix);
+
+            char symbolToFind = char.Parse(Console.ReadLine());
+
             int[] rowAndColumnOfWillingElement = new int[2];
 
+            bool isItFalse = FindExactSymbol(squareMatrix, symbolToFind, rowAndColumnOfWillingElement);
+
+            if (isItFalse)
+            {
+                Console.WriteLine("{0} does not occur in the matrix", symbolToFind);
+            }
+            else
+            {
+                Console.WriteLine($"({string.Join(", ", rowAndColumnOfWillingElement)})");
+            }
+        }
+
+        static void AddingElementsToMatrix(char[,] squareMatrix)
+        {
             for (int row = 0; row < squareMatrix.GetLength(0); row++)
             {
                 string characters = Console.ReadLine();
@@ -21,24 +40,24 @@
                     squareMatrix[row, col] = characters[col];
                 }
             }
+        }
 
-            char symbolToFind = char.Parse(Console.ReadLine());
-
+        static bool FindExactSymbol(char[,] squareMatrix, char symbolToFind, int[] rowAndColumnOfWillingElement)
+        {
             for (int row = 0; row < squareMatrix.GetLength(0); row++)
-            {  
+            {
                 for (int col = 0; col < squareMatrix.GetLength(1); col++)
                 {
                     if (symbolToFind == squareMatrix[row, col])
                     {
                         rowAndColumnOfWillingElement[0] = row;
-                        rowAndColumnOfWillingElement[1] = col;
-                        Console.WriteLine($"({string.Join(", ", rowAndColumnOfWillingElement)})");
-                        return;
+                        rowAndColumnOfWillingElement[1] = col;                     
+                        return false;
                     }
                 }
             }
 
-            Console.WriteLine("{0} does not occur in the matrix", symbolToFind);
+            return true;
         }
     }
 }
