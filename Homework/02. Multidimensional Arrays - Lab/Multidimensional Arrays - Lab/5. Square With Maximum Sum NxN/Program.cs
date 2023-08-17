9fdd550e580038b982bf85c0eb8b4677f9734683
Rumen <rumenstvass@gmail.com>
2023-08-18 00:14:29 +0100

    Solving Square With Maximum Sum Exercise

---
+++ Homework/02. Multidimensional Arrays - Lab/Multidimensional Arrays - Lab/5. Square With Maximum Sum NxN/Program.cs
@@ -0,0 +1,84 @@
+﻿/*
+ 3, 6
+1, 2, 3, 4, 5, 6
+7, 8, 9, 10, 11, 12
+13, 14, 15, 16, 17, 18
+ */
+
+using System.Text;
+
+namespace _5._Square_With_Maximum_Sum_NxN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] matrixArguments = input.Split(", ");
+
+            int matrixRows = int.Parse(matrixArguments[0]);
+            int matrixColumns = int.Parse(matrixArguments[1]);
+
+            int[,] matrix = new int[matrixRows, matrixColumns];
+
+            AddingElementsToMatrix(matrix);
+
+            int submatrixSize = int.Parse(Console.ReadLine());
+
+            FindBestSubMatrixSum(matrix, submatrixSize);
+        }
+
+        static void AddingElementsToMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string element = Console.ReadLine();
+                string[] matrixElements = element.Split(", ");
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = int.Parse(matrixElements[col]);
+                }
+            }
+        }
+
+        static void FindBestSubMatrixSum(int[,] matrix, int submatrixSize)
+        {
+            long currentSubmatrixSum = 0;
+
+            StringBuilder sb = new StringBuilder();
+            long bestSubmatrixSum = long.MinValue; ;
+            for (int row = 0; row <= matrix.GetLength(0) - submatrixSize; row++)
+            {
+                for (int col = 0; col <= matrix.GetLength(1) - submatrixSize; col++)
+                {
+                    int subMatrixCol = col;
+                     currentSubmatrixSum = SumElementsOfCurrentSubmatrix(matrix, submatrixSize, subMatrixCol + submatrixSize, row, col);
+                    if (currentSubmatrixSum > bestSubmatrixSum)
+                    {
+                        bestSubmatrixSum = currentSubmatrixSum;
+                    }
+                }
+            }
+
+            //Console.WriteLine(sb.ToString());
+            Console.WriteLine(bestSubmatrixSum);
+        }
+
+        static long SumElementsOfCurrentSubmatrix(int[,] matrix, int submatrixSize, int subMatrixCol, int row, int col)
+        {
+            
+            long currentSubmatrixSum = 0;
+
+            for (int currentRow = row; currentRow < submatrixSize; currentRow++)
+            {
+                for (int currentCol = col; currentCol < subMatrixCol; currentCol++)
+                {
+                    currentSubmatrixSum += matrix[currentRow, currentCol];
+                }
+            }
+
+            return currentSubmatrixSum;
+        }
+    }
+}
--- Homework/02. Multidimensional Arrays - Lab/Multidimensional Arrays - Lab/5. Square With Maximum Sum/Program.cs
+++ Homework/02. Multidimensional Arrays - Lab/Multidimensional Arrays - Lab/5. Square With Maximum Sum/Program.cs
@@ -1,10 +1,68 @@
-﻿namespace _5._Square_With_Maximum_Sum
+﻿using System.Text;
+
+namespace _5._Square_With_Maximum_Sum
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            string input = Console.ReadLine();
+            string[] matrixArguments = input.Split(", ");
+
+            int matrixRows = int.Parse(matrixArguments[0]);
+            int matrixColumns = int.Parse(matrixArguments[1]);
+
+            int[,] matrix = new int[matrixRows, matrixColumns];
+
+            AddingElementsToMatrix(matrix);
+
+            FindBestSubMatrixSum(matrix);
+        }
+
+        static void AddingElementsToMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string element = Console.ReadLine();
+                string[] matrixElements = element.Split(", ");
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = int.Parse(matrixElements[col]);
+                }
+            }
+        }
+
+        static void FindBestSubMatrixSum(int[,] matrix)
+        {
+            StringBuilder sb = new StringBuilder();
+            long bestSubmatrixSum = long.MinValue; ;
+            for (int row = 0; row <= matrix.GetLength(0) - 2; row++)
+            {
+                for (int col = 0; col <= matrix.GetLength(1) - 2; col++)
+                {
+                    long currentSubmatrixSum = matrix[row, col] + matrix[row, col + 1] +
+                        matrix[row + 1, col] + matrix[row + 1, col + 1];
+                    if (currentSubmatrixSum > bestSubmatrixSum)
+                    {
+                        bestSubmatrixSum = AppendBestSubMatrix(sb, matrix, row, col, bestSubmatrixSum, currentSubmatrixSum);
+                    }
+                }
+            }
+
+            Console.WriteLine(sb.ToString());
+            Console.WriteLine(bestSubmatrixSum);
+        }
+
+        static long AppendBestSubMatrix(StringBuilder sb, int[,] matrix, int row, int col, long bestSubmatrixSum, long currentSubmatrixSum)
+        {
+            sb.Clear();
+
+            sb.AppendLine($"{matrix[row, col]} {matrix[row, col + 1]}");
+            sb.Append($"{matrix[row + 1, col]} {matrix[row + 1, col + 1]}");
+            bestSubmatrixSum = currentSubmatrixSum;
+
+            return bestSubmatrixSum;
         }
     }
 }
