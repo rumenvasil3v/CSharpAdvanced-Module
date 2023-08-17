09c3464e228787c44a383ffc78fc2318a10eb270
Rumen <rumenstvass@gmail.com>
2023-08-18 00:14:29 +0100

    Solving Square With Maximum Sum Exercise

---
+++ Homework/02. Multidimensional Arrays - Lab/Multidimensional Arrays - Lab/5. Square With Maximum Sum NxN/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _5._Square_With_Maximum_Sum_NxN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] matrixArguments = input.Split(", ");
+
+            int matrixRows = int.Parse(matrixArguments[0]);
+            int matrixColumns = int.Parse(matrixArguments[1]);
+
+            int[,] matrix = new int[matrixRows, matrixColumns];
+
+            AddingElementsToMatrix(matrix);
+        }
+
+        static void AddingElementsToMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string element = Console.ReadLine();
+                string[] matrixElements = element.Split(", ");
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = int.Parse(matrixElements[col]);
+                }
+            }
+        }
+    }
+}
