f2188f87974b6202a827979858e8515f9bda3722
Rumen <rumenstvass@gmail.com>
2023-08-17 11:15:22 +0100

    Modifying files

--- Homework/02. Multidimensional Arrays - Lab/Multidimensional Arrays - Lab/1. Sum Matrix Elements/Program.cs
+++ Homework/02. Multidimensional Arrays - Lab/Multidimensional Arrays - Lab/1. Sum Matrix Elements/Program.cs
@@ -4,11 +4,7 @@
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            
-=======
             Console.WriteLine("Hello, World!");
->>>>>>> ea77eae09efd15b153c096f90d800a8e13643f07
         }
     }
 }
