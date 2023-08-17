84e14167801f7c3fde047bb2db12a3d563a1f287
Rumen <rumenstvass@gmail.com>
2023-08-17 23:36:55 +0100

    Debugging Exercises

--- Homework/02. Multidimensional Arrays - Lab/Multidimensional Arrays - Lab/1. Sum Matrix Elements- Exercise/Program.cs
+++ Homework/02. Multidimensional Arrays - Lab/Multidimensional Arrays - Lab/1. Sum Matrix Elements- Exercise/Program.cs
@@ -1,27 +1,44 @@
-﻿namespace _1._Sum_Matrix_Elements__Exercise
+﻿namespace _1._Sum_Matrix_Elements
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            // INPUT    
             string input = Console.ReadLine();
             string[] matrixArguments = input.Split(", ");
 
             int matrixRows = int.Parse(matrixArguments[0]);
             int matrixColumns = int.Parse(matrixArguments[1]);
 
+            // 3[rows], 6[columns]
             int[,] matrix = new int[matrixRows, matrixColumns];
 
+            // CODE LOGIC
             int sum = 0;
             for (int row = 0; row < matrix.GetLength(0); row++)
             {
-                string elements = Console.ReadLine();
-                string[] matrixElements = elements.Split(", ");
-
-                
+                sum = AddingElementsToEveryRow(matrix, row, sum);
             }
 
+            // OUTPUT
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
             Console.WriteLine(sum);
+        }
+
+        static int AddingElementsToEveryRow(int[,] matrix, int row, int sum)
+        {
+            string elements = Console.ReadLine();
+            string[] matrixElements = elements.Split(", ");
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                matrix[row, col] = int.Parse(matrixElements[col]);
+                sum += matrix[row, col];
+            }
+
+            return sum;
         }
     }
 }
