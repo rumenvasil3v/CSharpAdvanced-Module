a4f488826939df55f972cad0e46b3ae5c4880d73
Rumen <rumenstvass@gmail.com>
2023-08-17 11:52:47 +0100

    Solving Sum Matrix Elements Exercise

--- Homework/02. Multidimensional Arrays - Lab/Multidimensional Arrays - Lab/1. Sum Matrix Elements/Program.cs
+++ Homework/02. Multidimensional Arrays - Lab/Multidimensional Arrays - Lab/1. Sum Matrix Elements/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // INPUT    
+            string input = Console.ReadLine();
+            string[] matrixArguments = input.Split(", ");
+      
+            int matrixRows = int.Parse(matrixArguments[0]);
+            int matrixColumns = int.Parse(matrixArguments[1]);
+
+            // 3[rows], 6[columns]
+            int[,] matrix = new int[matrixRows, matrixColumns];
+
+            // CODE LOGIC
+            int sum = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+               sum = AddingElementsToEveryRow(matrix, row, sum);
+            }
+
+            // OUTPUT
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(sum);
+        }
+
+        static int AddingElementsToEveryRow(int[,] matrix, int row, int sum)
+        {
+            string elements = Console.ReadLine();
+            string[] matrixElements = elements.Split(", ");
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                matrix[row, col] = int.Parse(matrixElements[col]);
+                sum += matrix[row, col];
+            }
+
+            return sum;
         }
     }
 }
