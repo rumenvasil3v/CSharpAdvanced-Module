ea938def0229a7e9242e5b161c06a52c051b8109
Rumen <rumenstvass@gmail.com>
2023-08-17 12:13:47 +0100

    Debugging

---
+++ Homework/02. Multidimensional Arrays - Lab/Multidimensional Arrays - Lab/1. Sum Matrix Elements- Exercise/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _1._Sum_Matrix_Elements__Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] matrixArguments = input.Split(", ");
+
+            int matrixRows = int.Parse(matrixArguments[0]);
+            int matrixColumns = int.Parse(matrixArguments[1]);
+
+            int[,] matrix = new int[matrixRows, matrixColumns];
+
+            int sum = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string elements = Console.ReadLine();
+                string[] matrixElements = elements.Split(", ");
+
+                foreach (var m in matrix)
+                {
+                    matrix[row, m] = int.Parse(matrixElements[m]);
+                    sum += m;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
