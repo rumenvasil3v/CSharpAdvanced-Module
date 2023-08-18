a834ac473abb2767665e2ea3927043796ea8e7d6
Rumen <rumenstvass@gmail.com>
2023-08-18 20:07:34 +0100

    Solving Diagonal Difference Exercise

---
+++ Homework/02. Multidimensional Arrays - Exercise/Multidimensional Arrays - Exercise/1. Diagonal Difference/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace _1._Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeOfSquareMatrix = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[sizeOfSquareMatrix, sizeOfSquareMatrix];
+
+            AddElementsToMatrix(matrix);
+
+            int primaryDiagonal = SumSquareMatrixPrimaryDiagonal(matrix);
+            int secondaryDiagonal = SumSquareMatrixSecondaryDiagonal(matrix);
+
+            Console.WriteLine(Math.Abs(primaryDiagonal - secondaryDiagonal));
+        }
+
+        static void AddElementsToMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string elements = Console.ReadLine();
+                string[] matrixElements = elements.Split(' ');
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = int.Parse(matrixElements[col]);
+                }
+            }
+        }
+
+        static int SumSquareMatrixPrimaryDiagonal(int[,] matrix)
+        {
+            int primaryDiagonalSum = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                primaryDiagonalSum += matrix[row, row];
+            }
+
+            return primaryDiagonalSum;
+        }
+
+        static int SumSquareMatrixSecondaryDiagonal(int[,] matrix)
+        {
+            int secondaryDiagonalSum = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+               secondaryDiagonalSum += matrix[row, matrix.GetLength(1) - 1 - row];
+            }
+
+            return secondaryDiagonalSum;
+        }
+    }
+}
