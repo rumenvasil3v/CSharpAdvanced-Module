0c1982aef022e643263d5b6f4239d0b5b6260c45
Rumen <rumenstvass@gmail.com>
2023-08-18 12:58:44 +0100

    Solving Pascal Triangle

--- Homework/02. Multidimensional Arrays - Lab/Multidimensional Arrays - Lab/7. Pascal Triangle/Program.cs
+++ Homework/02. Multidimensional Arrays - Lab/Multidimensional Arrays - Lab/7. Pascal Triangle/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace _7._Pascal_Triangle
+﻿using System.Numerics;
+
+namespace _7._Pascal_Triangle
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int rows = int.Parse(Console.ReadLine());
+
+            BigInteger[][] jaggedArray = new BigInteger[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                jaggedArray[row] = new BigInteger[row + 1];
+                jaggedArray[row][0] = 1;
+                jaggedArray[row][jaggedArray[row].Length - 1] = 1;
+
+                AddElementsToEveryColumn(jaggedArray, row);
+
+                //Console.Write(new string(new string(' ', rows - row)));
+
+                Console.WriteLine(string.Join(" ", jaggedArray[row]));
+            }
+        }
+
+        static void AddElementsToEveryColumn(BigInteger[][] jaggedArray, int row)
+        {
+            for (int col = 0; col < jaggedArray[row].Length - 2; col++)
+            {
+                jaggedArray[row][col + 1] = jaggedArray[row - 1][col] + jaggedArray[row - 1][col + 1];
+            }
         }
     }
 }
