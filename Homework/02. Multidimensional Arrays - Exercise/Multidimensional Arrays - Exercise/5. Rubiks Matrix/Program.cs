3d67ecc30e0bf054a2c33257f7b9e9f21f42a5c1
Rumen <rumenstvass@gmail.com>
2023-08-28 20:53:55 +0300

    Solving Exercises

---
+++ Homework/02. Multidimensional Arrays - Exercise/Multidimensional Arrays - Exercise/5. Rubiks Matrix/Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+namespace _5._Rubiks_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] matrixArguments = input.Split(' ');
+
+            int matrixRows = int.Parse(matrixArguments[0]);
+            int matrixCols = int.Parse(matrixArguments[1]);
+
+            int[,] matrix = new int[matrixRows, matrixCols];
+            AddingElementsToMatrix(matrix);
+
+            int numberOfCommands = int.Parse(Console.ReadLine());
+            for (int n = 0; n < numberOfCommands; n++)
+            {
+                string movement = Console.ReadLine();
+                string[] movementArguments = movement.Split(' ');
+
+                int rowOrColToMove = 0;
+                int moves = 0;
+                switch (movementArguments[1])
+                {
+                    case "down":
+                        rowOrColToMove = int.Parse(movementArguments[0]);
+                        moves = int.Parse(movementArguments[2]);
+                        MatrixShuffleDown(matrix, moves, rowOrColToMove);
+                        break;
+                    case "up":
+                        rowOrColToMove = int.Parse(movementArguments[0]);
+                        moves = int.Parse(movementArguments[2]);
+                        MatrixShuffleUp(matrix, moves, rowOrColToMove);
+                        break;
+                    case "right":
+                        rowOrColToMove = int.Parse(movementArguments[0]);
+                        moves = int.Parse(movementArguments[2]);
+                        MatrixShuffleRight(matrix, moves, rowOrColToMove);
+                        break;
+                    case "left":
+                        rowOrColToMove = int.Parse(movementArguments[0]);
+                        moves = int.Parse(movementArguments[2]);
+                        MatrixShuffleLeft(matrix, moves, rowOrColToMove);
+                        break;
+                }  
+            }
+
+            RearrangeMatrix(matrix);
+        }
+
+        static void MatrixShuffleRight(int[,] matrix, int moves, int row)
+        {
+
+            while (moves  % matrix.GetLength(0) > 0)
+            {
+                int lastNumber = matrix[row, matrix.GetLength(1) - 1];
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    int currentNumber = matrix[row, col];
+
+                    matrix[row, col] = lastNumber;
+
+                    lastNumber = currentNumber;
+                }
+
+                moves--;
+            }
+        }
+
+        static void MatrixShuffleLeft(int[,] matrix, int moves, int row)
+        {
+
+            while (moves % matrix.GetLength(0) > 0)
+            {
+                int firstNumber = matrix[row, 0];
+
+                for (int col = matrix.GetLength(1) - 1; col >= 0; col--)
+                {
+                    int currentNumber = matrix[row, col];
+
+                    matrix[row, col] = firstNumber;
+
+                    firstNumber = currentNumber;
+                }
+
+                moves--;
+            }
+        }
+
+        static void AddingElementsToMatrix(int[,] matrix)
+        {
+            int number = 1;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = number++;
+                }
+            }
+        }
+
+        static void MatrixShuffleDown(int[,] matrix, int moves, int col)
+        {
+            while (moves % matrix.GetLength(1) > 0)
+            {
+                int lastNumber = matrix[matrix.GetLength(0) - 1, col];
+
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    int currentNumber = matrix[row, col];
+
+                    matrix[row, col] = lastNumber;
+
+                    lastNumber = currentNumber;
+                }
+
+                moves--;
+            }
+        }
+
+        static void MatrixShuffleUp(int[,] matrix, int moves, int col)
+        {
+
+            while (moves % matrix.GetLength(1) > 0)
+            {
+                int firstNumber = matrix[0, col];
+
+                for (int row = matrix.GetLength(0) - 1; row >= 0; row--)
+                {
+                    int currentNumber = matrix[row, col];
+
+                    matrix[row, col] = firstNumber;
+
+                    firstNumber = currentNumber;
+                }
+
+                moves--;
+            }
+        }
+
+        static void RearrangeMatrix(int[,] matrix)
+        {
+            int number = 1;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == number)
+                    {
+                        Console.WriteLine("No swap required");
+                    }
+                    else
+                    {
+                        int currentRow = row;
+                        int currentCol = col;
+                        int currentNumber = matrix[currentRow, currentCol];
+
+                        int rowToSwap = 0;
+                        int colToSwap = 0;
+                        bool swap = false;
+
+                        for (int r = 0; r < matrix.GetLength(0); r++)
+                        {
+                            for (int c = 0; c < matrix.GetLength(1); c++)
+                            {
+                                if (matrix[r, c] == number)
+                                {
+                                    rowToSwap = r;
+                                    colToSwap = c;
+                                    swap = true;
+                                    break;
+                                }
+                            }
+
+                            if (swap)
+                            {
+                                break;
+                            }
+                        }
+
+                        Console.WriteLine($"Swap ({currentRow}, {currentCol}) with ({rowToSwap}, {colToSwap})");
+                        matrix[currentRow, currentCol] = number;
+                        matrix[rowToSwap, colToSwap] = currentNumber;
+                    }
+
+                    number++;
+                }
+            }
+        }
+    }
+}
---
+++ Homework/02. Multidimensional Arrays - Exercise/Multidimensional Arrays - Exercise/6. Target Practice/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+
+namespace _6._Target_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] matrixArguments = input.Split(' ');
+
+            int matrixRows = int.Parse(matrixArguments[0]);
+            int matrixCols = int.Parse(matrixArguments[1]);
+
+            char[,] matrix = new char[matrixRows, matrixCols];
+            AddingElementsToMatrix(matrix);
+
+            PrintMatrix(matrix);
+
+            DestroySnake(matrix);
+
+            PrintMatrix(matrix);
+
+            MoveSymbols(matrix);
+
+            PrintMatrix(matrix);
+        }
+
+        static void MoveSymbols(char[,] matrix)
+        {
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    if (matrix[row, col] == ' ')
+                    {
+                        int bestSpace = FindBestSpace(matrix, col);
+
+                        for (int r = bestSpace; r >= 0; r--)
+                        {
+                            if (r == 0)
+                            {
+                                break;
+                            }
+
+                            char currentCharacter = matrix[r - 1, col];
+                            matrix[r, col] = currentCharacter;
+                            matrix[r - 1, col] = ' ';
+                        }
+
+                        Console.WriteLine();
+                        PrintMatrix(matrix);
+                    }
+                }
+            }
+        }
+
+        static int FindBestSpace(char[,] matrix, int col)
+        {
+            int best = int.MinValue;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+
+                if (matrix[row, col] == ' ')
+                {
+                    int currentSpaceRow = row;
+
+                    if (currentSpaceRow > best)
+                    {
+                        best = currentSpaceRow;
+                    }
+                }
+            }
+
+            return best;
+        }
+
+        static void DestroySnake(char[,] matrix)
+        {
+            string coordinates = Console.ReadLine();
+            string[] matrixCoordinates = coordinates.Split(" ");
+
+            int targetRow = int.Parse(matrixCoordinates[0]);
+            int targetCol = int.Parse(matrixCoordinates[1]);
+            int radius = int.Parse(matrixCoordinates[2]);
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    int dr = targetRow - row;
+                    int dc = targetCol - col;
+
+                    if (dr * dr + dc * dc <= radius * radius)
+                    {
+                        matrix[row, col] = ' ';
+                    }
+                }
+            }
+        }
+
+        static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static void AddingElementsToMatrix(char[,] matrix)
+        {
+            bool rightToLeft = true;
+            bool leftToRight = false;
+
+            string snake = Console.ReadLine();
+            int count = 0;
+
+            for (int row = matrix.GetLength(0) - 1; row >= 0; row--)
+            {
+                if (rightToLeft)
+                {
+                    for (int col = matrix.GetLength(1) - 1; col >= 0; col--)
+                    {
+                        matrix[row, col] = snake[count++];
+
+                        if (count == snake.Length)
+                        {
+                            count = 0;
+                        }
+                    }
+
+                    rightToLeft = false;
+                    leftToRight = true;
+                }
+                else if (leftToRight)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        matrix[row, col] = snake[count++];
+
+                        if (count == snake.Length)
+                        {
+                            count = 0;
+                        }
+                    }
+
+                    rightToLeft = true;
+                    leftToRight = false;
+                }
+            }
+        }
+
+    }
+}
