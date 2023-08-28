6fa492ec9ebaf6659cc4b1bc9a8111985eab3d55
Rumen <rumenstvass@gmail.com>
2023-08-28 17:10:10 +0300

    Solving Exercises

---
+++ Homework/02. Multidimensional Arrays - Exercise/Multidimensional Arrays - Exercise/11. Matrix of Palindromes/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _11._Matrix_of_Palindromes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] matrixArguments = input.Split();
+
+            int matrixRows = int.Parse(matrixArguments[0]);
+            int matrixCols = int.Parse(matrixArguments[1]);
+
+            string[,] matrix = new string[matrixRows, matrixCols];
+
+            AddingElementsToMatrix(matrix);
+            PrintMatrix(matrix);
+        }
+
+        static void AddingElementsToMatrix(string[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char firstLastCharacter = (char)(97 + row);
+                char middleCharacter = (char)(97 + row);
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    string text = firstLastCharacter.ToString() + middleCharacter.ToString() + firstLastCharacter.ToString();
+
+                    matrix[row, col] = text;
+
+                    middleCharacter = (char)(97 + col + 1 + row);
+                }
+            }
+        }
+
+        static void PrintMatrix(string[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row, col]} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Homework/02. Multidimensional Arrays - Lab/Multidimensional Arrays - Lab/8. Group Numbers/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _8._Group_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] text = input.Split(", ");
+
+            int[] numbers = text.Select(int.Parse).ToArray();
+
+            GetCountOfRemainders(numbers);
+        }
+
+        static void GetCountOfRemainders(int[] numbers)
+        {
+            int[] countOfRemainders = new int[3];
+
+            foreach (int number in numbers)
+            {
+                if (Math.Abs(number) % 3 == 0)
+                {
+                    countOfRemainders[0]++;
+                }
+                else if (Math.Abs(number) % 3 == 1)
+                {
+                    countOfRemainders[1]++;
+                }
+                else if (Math.Abs(number) % 3 == 2)
+                {
+                    countOfRemainders[2]++;
+                }
+            }
+
+            AllocateNumbers(countOfRemainders, numbers);
+        }
+
+        static void AllocateNumbers(int[] countOfRemainders, int[] numbers)
+        {
+            int[][] jaggedArray = new int[3][];
+            int indexesZeroRemainder = 0;
+            int indexesOneRemainder = 0;
+            int indexesTwoRemainder = 0;
+
+            for (int row = 0; row < jaggedArray.Length; row++)
+            {
+                jaggedArray[row] = new int[countOfRemainders[row]];
+            }
+
+            foreach (int number in numbers)
+            {
+                if (Math.Abs(number) % 3 == 0)
+                {
+                    jaggedArray[0][indexesZeroRemainder++] = number;
+                }
+                else if (Math.Abs(number) % 3 == 1)
+                {
+                    jaggedArray[1][indexesOneRemainder++] = number;
+                }
+                else if (Math.Abs(number) % 3 == 2)
+                {
+                    jaggedArray[2][indexesTwoRemainder++] = number;
+                }
+            }
+
+            PrintJaggedArray(jaggedArray);
+        }
+
+        static void PrintJaggedArray(int[][] jaggedArray)
+        {
+            for (int row = 0; row < jaggedArray.Length; row++)
+            {
+                Console.WriteLine(string.Join(' ', jaggedArray[row]));
+            }
+        }
+    }
+}
