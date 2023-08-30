615b1bc76a9c5474889100710435a9927fa19f7b
Rumen <rumenstvass@gmail.com>
2023-08-30 17:48:22 +0300

    Solving Exercises

---
+++ Homework/03. Sets and Dictionaries Advanced - Lab/Sets and Dictionaries Advanced - Lab/01. Count Same Values in Array/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Same_Values_in_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<double, int> realNumbersOccurences = new Dictionary<double, int>();
+
+            string input = Console.ReadLine();
+            string[] inputNumbers = input.Split(' ');
+            double[] numbers = inputNumbers.Select(double.Parse).ToArray();
+
+            IterateThroughNumbers(numbers, realNumbersOccurences);
+            PrintDictionary(realNumbersOccurences);
+        }
+
+        static void PrintDictionary(Dictionary<double, int> realNumbersOccurences)
+        {
+            foreach (KeyValuePair<double, int> kvp in realNumbersOccurences)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+            }
+        }
+
+        static void IterateThroughNumbers(double[] numbers, Dictionary<double, int> realNumberOccurences)
+        {
+            for (int n = 0; n < numbers.Length; n++)
+            {
+                double currentNumber = numbers[n];
+
+                if (!realNumberOccurences.ContainsKey(currentNumber))
+                {
+                    realNumberOccurences.Add(currentNumber, 1);
+                    continue;
+                }
+
+                realNumberOccurences[currentNumber]++;
+            }
+        }
+    }
+}
---
+++ Homework/03. Sets and Dictionaries Advanced - Lab/Sets and Dictionaries Advanced - Lab/04. Product Shop/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Product_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, double>> shops = new Dictionary<string, Dictionary<string, double>>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "Revision")
+            {
+                string[] shopArguments = command.Split(", ");
+
+                string currentShop = shopArguments[0];
+                string productShop = shopArguments[1];
+                double productPrice = double.Parse(shopArguments[2]);
+
+                Shop shop = new Shop(currentShop, productShop, productPrice);
+
+                shop.AddingProducts(shops);
+            }
+
+            foreach (var shop in shops.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{shop.Key}->");
+
+                foreach (var product in shop.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
+        }
+    }
+
+    public class Shop
+    {
+
+        public Shop(string name, string product, double productPrice)
+        {
+            this.Name = name;
+            this.Product = product;
+            this.ProductPrice = productPrice;
+        }
+
+        public string Name { get; set; }
+
+        public string Product { get; set; }
+
+        public double ProductPrice { get; set; }
+
+        public void AddingProducts(Dictionary<string, Dictionary<string, double>> shops)
+        {
+            if (!shops.ContainsKey(this.Name))
+            {
+                shops.Add(this.Name, new Dictionary<string, double>() { { this.Product, this.ProductPrice } });
+                return;
+            }
+
+            shops[this.Name].Add(this.Product, this.ProductPrice);
+        }
+    }
+}
