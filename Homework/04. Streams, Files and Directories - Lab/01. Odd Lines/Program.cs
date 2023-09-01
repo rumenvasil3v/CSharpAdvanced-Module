6a86ed2fd2c82a5878a4124b159f50879d2d0ae7
Rumen <rumenstvass@gmail.com>
2023-09-02 01:32:44 +0300

    Solving Exercises

---
+++ Homework/04. Streams, Files and Directories - Lab/01. Odd Lines/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace OddLines
+{
+    using System.IO;
+
+    public class OddLines
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = @"C:\temp\input.txt";
+            string outputFilePath = @"output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            using StreamReader reader = new StreamReader(inputFilePath);
+
+            string line = reader.ReadLine();
+
+            int index = 0;
+            using StreamWriter writer = new StreamWriter(outputFilePath);
+            while (line != null)
+            {
+                if (index % 2 == 1)
+                {
+                    writer.WriteLine(line);
+                }
+
+                index++;
+                line = reader.ReadLine();
+            }
+        }
+    }
+}
---
+++ Homework/04. Streams, Files and Directories - Lab/02. Line Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = @"C:\temp\input.txt";
+            string outputFilePath = @"output.txt";
+
+            RewriteFileWithLineNumbers(inputFilePath, outputFilePath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            using StreamReader reader = new StreamReader(inputFilePath);
+            {
+                int numberLine = 1;
+                string line;
+
+                using StreamWriter writer = new StreamWriter(outputFilePath);
+                {
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        writer.WriteLine("{0}. {1}", numberLine, line);
+                        numberLine++;
+                    }
+                }
+            }
+        }
+    }
+}
