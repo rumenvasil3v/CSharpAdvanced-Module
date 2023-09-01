1d267708a864e710a92f0d1c6b4daf629abda33b
Rumen <rumenstvass@gmail.com>
2023-09-02 01:34:36 +0300

    Solving Exercises

---
+++ Homework/04. Streams, Files and Directories - Lab/03. Word Count/WordCount.cs
@@ -0,0 +1,57 @@
+﻿namespace WordCount
+{
+
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class WordCount
+    {
+        static void Main(string[] args)
+        {
+            string wordsFilePath = @"C:\temp\words.txt";
+            string textFilePath = @"C:\temp\text.txt";
+            string outputFilePath = @"output.txt";
+
+            CalculateWordCounts(wordsFilePath, textFilePath, outputFilePath);
+        }
+
+        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
+        {
+            string[] words;
+
+            using StreamReader reader = new StreamReader(wordsFilePath);
+            {
+                words = reader.ReadLine().Split(' ');
+            }
+
+            Regex regex = new Regex(@"\bquick\b|\bfault\b|\bis\b", RegexOptions.IgnoreCase);
+
+            Dictionary<string, int> wordOccurences = new Dictionary<string, int>();
+            using StreamReader textReader = new StreamReader(textFilePath);
+            {
+                string line;
+                while ((line = textReader.ReadLine()) != null)
+                {
+                    MatchCollection matches = regex.Matches(line);
+                    foreach (Match match in matches)
+                    {
+                        string currentMatch = match.Value.ToLower();
+
+                        if (!wordOccurences.ContainsKey(currentMatch))
+                        {
+                            wordOccurences.Add(currentMatch, 0);
+                        }
+
+                        wordOccurences[currentMatch]++;
+                    }
+                }
+            }
+
+            using StreamWriter writer = new StreamWriter(outputFilePath);
+            foreach (var kvp in wordOccurences.OrderByDescending(x => x.Value))
+            {
+                writer.WriteLine("{0} - {1}", kvp.Key, kvp.Value);
+            }
+        }
+    }
+}
