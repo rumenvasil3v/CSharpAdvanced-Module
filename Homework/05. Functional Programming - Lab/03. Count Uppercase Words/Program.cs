7715c03f4b98510acfc5198508892afd85d55cc5
rumenvasil3v <rumenstvass@gmail.com>
2023-09-10 13:07:42 +0300

    Solving Exercises

--- Homework/05. Functional Programming - Lab/03. Count Uppercase Words/Program.cs
+++ Homework/05. Functional Programming - Lab/03. Count Uppercase Words/Program.cs
@@ -1,11 +1,27 @@
-﻿namespace _03._Count_Uppercase_Words
+﻿using System.Diagnostics.SymbolStore;
+
+namespace _03._Count_Uppercase_Words
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Func<string, bool> func = x => { return char.IsUpper(x[0]); };
-            var lineOfText = Console.ReadLine().Split(' ').Where(func);
+            //Predicate<string> predicate = x => x[0] == x.ToUpper()[0];
+            //string[] lineOfText = Console
+            //    .ReadLine()
+            //    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            //    .Where(w => predicate(w))
+            //    .ToArray();
+
+            //Console.WriteLine(string.Join(Environment.NewLine, lineOfText));
+
+            Func<string, bool> func = x => char.IsUpper(x[0]);
+            string[] lineOfText = Console
+                 .ReadLine()
+                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                 .Where(func)
+                 .ToArray();
+
             Console.WriteLine(string.Join(Environment.NewLine, lineOfText));
         }
     }
