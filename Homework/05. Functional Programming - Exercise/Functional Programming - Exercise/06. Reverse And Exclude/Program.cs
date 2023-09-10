2c86995e52dc66fbde086aa3e33f79d97f9c1ec3
rumenvasil3v <rumenstvass@gmail.com>
2023-09-10 15:49:57 +0300

    Solving Exercises

---
+++ Homework/05. Functional Programming - Exercise/Functional Programming - Exercise/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _06._Reverse_And_Exclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console
+                .ReadLine()
+                .Split(' ')
+                .Select(int.Parse);
+
+            int divisor = int.Parse(Console.ReadLine());
+            Predicate<int> predicate = n => !(n % divisor == 0);
+
+            numbers = numbers.Reverse();
+            var filteredNumbers = numbers.Where(n => predicate(n));
+
+            Console.WriteLine(string.Join(" ", filteredNumbers));
+        }
+    }
+}
