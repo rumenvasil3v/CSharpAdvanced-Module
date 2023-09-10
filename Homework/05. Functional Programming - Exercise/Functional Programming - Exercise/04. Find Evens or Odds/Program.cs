89d61e2d62e5ce318baab74951076f33eb7b80d3
rumenvasil3v <rumenstvass@gmail.com>
2023-09-10 13:59:24 +0300

    Solving Exercises

---
+++ Homework/05. Functional Programming - Exercise/Functional Programming - Exercise/04. Find Evens or Odds/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+
+namespace _04._Find_Evens_or_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] upperAndLowerBoundArguments = input
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            int lowerBound = int.Parse(upperAndLowerBoundArguments[0]);
+            int upperBound = int.Parse(upperAndLowerBoundArguments[1]);
+
+            string command = Console.ReadLine();
+            Predicate<int> predicate = EvenOrOdd(command);
+
+            List<int> numbers = new List<int>();
+            for (int n = lowerBound; n <= upperBound; n++)
+            {
+                if (predicate(Math.Abs(n)))
+                {
+                    numbers.Add(n);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        static Predicate<int> EvenOrOdd(string command)
+        {
+            switch (command)
+            {
+                case "odd":
+                    return n => n % 2 == 1;
+                case "even":
+                    return n => n % 2 == 0;
+                default:
+                    return n => false;
+            }
+        }
+    }
+}
---
+++ Homework/05. Functional Programming - Exercise/Functional Programming - Exercise/05. Applied Arithmetics/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace _05._Applied_Arithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console
+                .ReadLine()
+                .Split(' ')
+                .Select(int.Parse);
+
+            string command = Console.ReadLine();
+            while (true)
+            {
+                if (command == "end")
+                {
+                    break;
+                }
+
+                Func<int, int> func = ArithmeticOperation(command);
+
+                switch (command)
+                {
+                    case "add":
+                        numbers = numbers.Select(func);
+                        break;
+                    case "multiply":
+                        numbers = numbers.Select(func);
+                        break;
+                    case "subtract":
+                        numbers = numbers.Select(func);
+                        break;
+                    case "print":
+                        foreach (var item in numbers)
+                        {
+                            Console.Write(func(item) + " ");
+                        }
+
+                        Console.WriteLine();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid command!");
+                        break;
+
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+
+        static Func<int, int> ArithmeticOperation(string command)
+        {
+            switch (command)
+            {
+                case "add":
+                    return n => n + 1;
+                case "multiply":
+                    return n => n * 2;
+                case "subtract":
+                    return n => n - 1;
+                case "print":
+                    return n => n;
+                default:
+                    throw new ArgumentException(command);
+            }
+        }
+    }
+}
