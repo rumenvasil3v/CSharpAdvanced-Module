205ad5cc34f654af32125bf3e7add13ff998e1cf
rumenvasil3v <rumenstvass@gmail.com>
2023-09-10 18:08:47 +0300

    Solving Exercises

--- Homework/05. Functional Programming - Exercise/Functional Programming - Exercise/10. The Party Reservation Filter Module/Program.cs
+++ Homework/05. Functional Programming - Exercise/Functional Programming - Exercise/10. The Party Reservation Filter Module/Program.cs
@@ -4,7 +4,54 @@
     {
         static void Main(string[] args)
         {
-            
+            var invitedPeople = Console.ReadLine().Split(' ').ToList();
+
+            var filters = new List<string>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "Print")
+            {
+                string[] filterArguments = command.Split(';');
+
+                switch (filterArguments[0])
+                {
+                    case "Add filter":
+                        filters.Add($"{filterArguments[1]};{filterArguments[2]}");
+                        break;
+                    case "Remove filter":
+                        filters.Remove($"{filterArguments[1]};{filterArguments[2]}");
+                        break;
+                }
+            }
+
+            foreach (string filter in filters)
+            {
+                string filterType = filter.Split(';')[0];
+                string filterValue = filter.Split(';')[1];
+
+                Predicate<string> predicate = Filter(filterType, filterValue);
+
+                invitedPeople = invitedPeople.Where(p => !predicate(p)).ToList();
+            }
+
+            Console.WriteLine(string.Join(' ', invitedPeople));
+        }
+
+        static Predicate<string> Filter(string filter, string filterValue)
+        {
+            switch (filter)
+            {
+                case "Starts with":
+                    return f => f.StartsWith(filterValue);
+                case "Ends with":
+                    return f => f.EndsWith(filterValue);
+                case "Length":
+                    return f => f.Length == int.Parse(filterValue);
+                case "Contains":
+                    return f => f.Contains(filterValue);
+                default:
+                    throw new ArgumentException($"Invalid filter -> {filter}");
+            }
         }
     }
 }
