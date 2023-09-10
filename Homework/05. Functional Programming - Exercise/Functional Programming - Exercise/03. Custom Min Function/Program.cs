a0f03ba7d63a6e89559ee460761e57369a38b10c
rumenvasil3v <rumenstvass@gmail.com>
2023-09-10 13:59:24 +0300

    Solving Exercises

---
+++ Homework/05. Functional Programming - Exercise/Functional Programming - Exercise/03. Custom Min Function/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _03._Custom_Min_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console
+                .ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            Func<int, int> func = x => x;
+            Console.WriteLine(numbers.Min(func));
+
+            //int minNumber = Min2(numbers, x => x);
+            //Console.WriteLine(minNumber);
+        }
+
+        //static int Min2(List<int> numbers, Func<int, double> func)
+        //{
+        //    int minNumber = int.MaxValue;
+
+        //    for (int n = 0; n < numbers.Count; n++)
+        //    {
+        //        double currentNumber = func(numbers[n]);
+
+        //        if (currentNumber < minNumber)
+        //        {
+        //            minNumber = numbers[n];
+        //        }
+        //    }
+
+        //    return minNumber;
+        //}
+    }
+}
