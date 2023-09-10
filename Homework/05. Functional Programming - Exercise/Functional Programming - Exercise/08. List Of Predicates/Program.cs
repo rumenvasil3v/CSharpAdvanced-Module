fc3891ec0d20626080a022352afafb178bd63049
rumenvasil3v <rumenstvass@gmail.com>
2023-09-10 16:13:41 +0300

    Solving Exercises

---
+++ Homework/05. Functional Programming - Exercise/Functional Programming - Exercise/08. List Of Predicates/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _08._List_Of_Predicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int endOfRange = int.Parse(Console.ReadLine());
+
+            var numbers = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse);
+
+            List<int> validNumbers = new List<int>(); 
+            for (int n = 1; n <= endOfRange; n++)
+            {
+                Predicate<int> predicate = x => n % x == 0;
+
+                bool isDivisible = numbers.All(x => predicate(x));
+                if (isDivisible)
+                {
+                    validNumbers.Add(n);
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', validNumbers));
+        }
+    }
+}
