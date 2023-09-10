f2861896d33e66ff9a0656b17ad9eb071cac292c
rumenvasil3v <rumenstvass@gmail.com>
2023-09-10 13:16:11 +0300

    Solving Exercises

---
+++ Homework/05. Functional Programming - Exercise/Functional Programming - Exercise/02. Knights of Honor/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _02._Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = Console.ReadLine().Split(' ');
+            Action<string> action = name => Console.WriteLine($"Sir {name}");
+
+            foreach (var name in names)
+            {
+                action(name);
+            }
+        }
+    }
+}
