89367415b3a595e7280df04e819263c11f31e595
rumenvasil3v <rumenstvass@gmail.com>
2023-09-13 22:49:44 +0300

    Solving Exercises

---
+++ Homework/05. Functional Programming - Exercise/Recursion and Backtracking - Lab/05. Paths in Labyrinth/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _05._Paths_in_Labyrinth
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = { 1, 3 };
+            int[] secondArray = { 2, 4 };
+            int[] newArray = firstArray.Concat(secondArray).ToArray();
+
+            Array.Sort(newArray);
+
+
+            double median = (newArray[newArray.Length / 2 - 1] + newArray[newArray.Length / 2]) / 2.0;
+            Console.WriteLine(median);
+
+        }
+    }
+}
