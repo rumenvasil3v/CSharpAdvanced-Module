bbfc6a744a36a9e468b8c4ca9b7d950729e99677
rumenvasil3v <rumenstvass@gmail.com>
2023-09-18 16:44:09 +0300

    Solving Exercises from Defining Classes lection

--- Homework/06. Defining Classes - Exercise/Defining Classes - Exercise/08. Car Salesman/Car.cs
+++ Homework/06. Defining Classes - Exercise/Defining Classes - Exercise/08. Car Salesman/Car.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,12 +10,16 @@ namespace DefiningClasses
     public class Car
     {
 
-        public Car(string model, Engine engine, int weight, string color)
+        public Car(int weight, string color)
+        {
+            this.Weight = weight;
+            this.Color = color;
+        }
+
+        public Car(string model, Engine engine, int weight, string color) : this(weight, color)
         {
             this.Model = model;
             this.Engine = engine;
-            this.Weight = weight;
-            this.Color = color;
         }
 
         public string Model { get; set; }
@@ -24,5 +29,28 @@ namespace DefiningClasses
         public int Weight { get; set; }
 
         public string Color { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{this.Model}:");
+            sb.AppendLine($"  {this.Engine.Model}:");
+            sb.AppendLine($"    Power: {this.Engine.Power}");
+
+            var engineDisplacement = this.Engine.Displacement == 0 ? "n/a" : $"{this.Engine.Displacement}";
+            sb.AppendLine($"    Displacement: {engineDisplacement}");
+
+            var engineEfficiency = this.Engine.Efficiency == string.Empty ? "n/a" : $"{this.Engine.Efficiency}";
+            sb.AppendLine($"    Efficiency: {engineEfficiency}");
+
+            var carWeight = this.Weight == 0 ? "n/a" : $"{this.Weight}";
+            sb.AppendLine($"  Weight: {carWeight}");
+
+            var carColor = this.Color == string.Empty ? "n/a" : $"{this.Color}";
+            sb.Append($"  Color: {carColor}");
+
+            return sb.ToString();
+        }
     }
 }
--- Homework/06. Defining Classes - Exercise/Defining Classes - Exercise/08. Car Salesman/StartUp.cs
+++ Homework/06. Defining Classes - Exercise/Defining Classes - Exercise/08. Car Salesman/StartUp.cs
@@ -4,6 +4,23 @@
     {
         static void Main(string[] args)
         {
+            List<Engine> engines = GetEngines();
+
+            List<Car> cars = GetCars(engines);
+
+            PrintCars(cars);
+        }
+
+        static void PrintCars(List<Car> cars)
+        {
+            foreach (Car car in cars)
+            {
+                Console.WriteLine(car.ToString());
+            }
+        }
+
+        static List<Engine> GetEngines()
+        {
             List<Engine> engines = new List<Engine>();
 
             int numberOfEngines = int.Parse(Console.ReadLine());
@@ -12,16 +29,44 @@
                 string[] engineInfo = Console
                     .ReadLine()
                     .Split(' ');
-                
+
                 string engineModel = engineInfo[0];
                 int enginePower = int.Parse(engineInfo[1]);
-                int engineDisplacement = int.Parse(engineInfo[2]);
-                string engineEfficiency = engineInfo[3];
 
-                Engine engine = new Engine(engineModel, enginePower, engineDisplacement, engineEfficiency);
+                string engineEfficiency = string.Empty;
+                if (engineInfo.Length > 2)
+                {
+                    bool isItTrue = int.TryParse(engineInfo[2], out int engineDisplacement);
+                    if (isItTrue)
+                    {
+                        if (engineInfo.Length > 3)
+                        {
+                            engineEfficiency = engineInfo[3];
+                        }
+                    }
+                    else
+                    {
+                        engineEfficiency = engineInfo[2];
+                    }
 
-                engines.Add(engine);
+                    Engine engine = new Engine(engineModel, enginePower, engineDisplacement, engineEfficiency);
+
+                    engines.Add(engine);
+                }
+                else
+                {
+                    Engine engine = new Engine(engineModel, enginePower, 0, string.Empty);
+
+                    engines.Add(engine);
+                }
             }
+
+            return engines;
+        }
+
+        static List<Car> GetCars(List<Engine> engines)
+        {
+            List<Car> cars = new List<Car>();
 
             int numberOfCars = int.Parse(Console.ReadLine());
             for (int n = 0; n < numberOfCars; n++)
@@ -30,8 +75,38 @@
                     .ReadLine()
                     .Split(' ');
 
+                string carModel = carInfo[0];
 
+                string carEngine = carInfo[1];
+                Engine engine = engines.FirstOrDefault(e => e.Model == carEngine);
+
+                string carColor = string.Empty;
+                if (carInfo.Length > 2)
+                {
+                    bool isIsTrue = int.TryParse(carInfo[2], out int carWeight);
+                    if (isIsTrue)
+                    {
+                        if (carInfo.Length > 3)
+                        {
+                            carColor = carInfo[3];
+                        }
+                    }
+                    else
+                    {
+                        carColor = carInfo[2];
+                    }
+
+                    Car car = new Car(carModel, engine, carWeight, carColor);
+                    cars.Add(car);
+                }
+                else
+                {
+                    Car car = new Car(carModel, engine, 0, string.Empty);
+                    cars.Add(car);
+                }
             }
+
+            return cars;
         }
     }
 }
