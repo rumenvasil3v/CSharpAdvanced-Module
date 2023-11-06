64ca7fc331f56a9ba1a8213bcb1079fb51713f42
Zenyx Zenyx <Zenyx@students.softuni.bg>
2023-11-07 00:08:30 +0200

    Solving Inheritance Problems

--- CSharpOOPCourse-Homework/01. Inheritance-Lab/Inheritance - Lab/03.HierarchicalInheritance/Program.cs
+++ CSharpOOPCourse-Homework/01. Inheritance-Lab/Inheritance - Lab/03.HierarchicalInheritance/Program.cs
@@ -2,13 +2,16 @@
 {
     public class StartUp
     {
-        
+
         static void Main(string[] args)
         {
             Dog dog = new Dog();
-
             dog.Bark();
             dog.Eat();
+
+            Cat cat = new Cat();
+            cat.Meow();
+            cat.Eat();
         }
     }
 }
